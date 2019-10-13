71eac37fa94394ac89a797738682fb11136c3ab3
sy0507 <1872440473@qq.com>
2019-10-13 14:29:29 +0800

    docker部署

--- src/main/java/com/example/thirteen/ThirteenApplication.java
+++ src/main/java/com/example/thirteen/ThirteenApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ThirteenApplication {
 
+
   public static void main(String[] args) {
     SpringApplication.run(ThirteenApplication.class, args);
   }
