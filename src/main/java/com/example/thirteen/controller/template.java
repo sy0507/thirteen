6deace925f2021535d31a88464c11d827f460025
sy0507 <1872440473@qq.com>
2019-10-14 20:27:46 +0800

    前端上传

---
+++ src/main/java/com/example/thirteen/controller/template.java
@@ -0,0 +1,35 @@
+package com.example.thirteen.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author haha
+ */
+@Controller
+public class template {
+
+
+  @GetMapping("/login")
+  public  String login(){
+    return "html/login.html";
+  }
+
+  @GetMapping("/current.html")
+  public  String c(){
+    return "html/current.html";
+  }
+
+  @GetMapping("/menu.html")
+  public  String m(){
+    return "html/menu.html";
+  }
+
+  @GetMapping("/sort.html")
+  public  String s(){
+    return "html/sort.html";
+  }
+
+
+}
